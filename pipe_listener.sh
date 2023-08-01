6a995754b130ba8647994f62c74571dbdec0d685
seshAlkimi <125999511+seshAlkimi@users.noreply.github.com>
2023-08-01 18:21:31 +0100

    Update pipe_listener.sh

--- pipe_listener.sh
+++ pipe_listener.sh
@@ -1,3 +1,20 @@
+PIPE_DIR=/app/docker_pipes
+PIPE_FILE=/app/docker_pipes/project_app_pipe
+if [ -d "$PIPE_DIR" ] 
+then
+        if [ -e "$PIPE_FILE" ]
+        then
+                echo "Pipe Exists"
+        else
+                mkfifo $PIPE_FILE
+                echo "PIPE Created"
+        fi
+else
+        mkdir /app/docker_pipes
+        mkfifo $PIPE_FILE
+        echo "PIPE Created"
+fi
+
 while true;
-  do eval "$(cat /app/docker_pipes/project_app_pipe)" ;
+        do eval "$(cat $PIPE_FILE)" ;
 done
