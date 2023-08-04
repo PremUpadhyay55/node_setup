5537468e5833f285bff2c394a2141a79707ea663
Sesha.andukuri@alkimi.org <sesha.andukuri@alkimi.org>
2023-08-04 08:32:18 +0100

    nginx details added

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+echo "Installing NGINX"
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-available/*
+sudo cp ~/nms/nms.cfg /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/nms.cfg /etc/nginx/sites-enabled/
+sudo systemctl start nginx
 echo "First Checking Pipe and starting"
 nohup ./pipe_listener.sh &
 echo "Starting the NMS Docker "
