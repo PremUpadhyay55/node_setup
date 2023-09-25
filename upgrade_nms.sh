aabcace6fbba4ba71f70202247b32c872e4ee1a1
Sesha.andukuri@alkimi.org <sesha.andukuri@alkimi.org>
2023-09-25 13:52:12 +0100

    upgrade_nms.sh added

--- upgrade_nms.sh
+++ upgrade_nms.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-shopt -s extglob
+shopt -s extglob  ## this is to add exlude a folder in a cp command
 cd /home/ubuntu/node_setup
-##sudo docker-compose down
+sudo docker-compose down
 server_pid="$(<webserver_pid.txt)"
 echo "SERVICER PID IS $server_pid"
 folder_name="nms"$(date +%Y%m%d%H%M%S)
@@ -9,7 +9,7 @@ mkdir -p backup/"$folder_name"
 cp -rp /home/ubuntu/node_setup/!(backup) backup/"$folder_name"
 git clone https://github.com/Alkimi-Exchange/node_setup.git
 wget --backups=1 https://d1xjh92lb8fey3.cloudfront.net/NMS-Update/dev/nms_web_server
-sudo docker-compose up -d
 sudo chmod 755 nms_web_server
 nohup ./nms_web_sever &
+sudo docker-compose up -d
 echo $! > webserver_pid.txt
